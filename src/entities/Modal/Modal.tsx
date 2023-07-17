f12836c420693a4df220f1710106d247f1a03677
Binatik <sadxas999@mail.ru>
2023-07-17 21:25:25 +0300

    добавлена анимация transition - group

--- src/entities/Modal/Modal.tsx
+++ src/entities/Modal/Modal.tsx
@@ -2,34 +2,28 @@ import ReactDOM from 'react-dom'
 import { IModalProps } from './Modal.interface'
 import styles from './Modal.module.css'
 import { ButtonClose } from '../Buttons/Button.close/ButtonClose'
+import { CSSTransition } from 'react-transition-group'
 
 function Modal({ children, isActive, setIsActive }: IModalProps) {
-	console.log(isActive)
-	function setModal() {
-		return (
-			isActive && (
-				<div onClick={() => setIsActive(false)} className={styles.modal}>
-					<ButtonClose
-						onClick={() => setIsActive(false)}
-						className={styles.modal_close}
-						mode="primary"
-						size="medium"
-					/>
-					<div
-						onClick={(event) => event.stopPropagation()}
-						className={styles.modal_inner}>
-						{children}
-					</div>
-				</div>
-			)
-		)
-	}
-
 	const root = document.getElementById('root')
-	if (!root) return
+	if (!root) return null
 
-	const modal = setModal()
-	return ReactDOM.createPortal(modal, root)
+	return ReactDOM.createPortal(
+		<CSSTransition in={isActive} timeout={300} classNames={'modal_transition'} unmountOnExit>
+			<div onClick={() => setIsActive(false)} className={styles.modal}>
+				<ButtonClose
+					onClick={() => setIsActive(false)}
+					className={styles.modal_close}
+					mode="primary"
+					size="medium"
+				/>
+				<div onClick={(event) => event.stopPropagation()} className={styles.modal_inner}>
+					{children}
+				</div>
+			</div>
+		</CSSTransition>,
+		root
+	)
 }
 
 export { Modal }
